fd64237d82cd7938b38576b38666ef1a82b2546b
Sander van Dorsten <Sander-van.Dorsten@klm.com>
2019-06-09 17:38:38 +0200

    push

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
-              'sphinxcontrib.napoleon'
+              # 'sphinxcontrib.napoleon'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
